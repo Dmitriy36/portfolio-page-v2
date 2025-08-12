b1f04d77f3845aaf2e066ab5eff9112453ece083
Ignatius_Reilly <dmitriy_xix@hotmail.com>
2025-08-12 05:07:07 -0400

    more work

--- script.js
+++ script.js
@@ -1,20 +1,17 @@
 function SwitchTheme() {
-  let darkMode = localStorage.getItem("darkMode");
-  darkMode !== "active" ? EnableDarkMode() : DisableDarkMode();
+  // let darkMode = localStorage.getItem("darkMode");
+  document.body.className !== "darkmode" ? EnableDarkMode() : DisableDarkMode();
+  // darkMode !== "active" ? EnableDarkMode() : DisableDarkMode();
 }
 
 function EnableDarkMode() {
   const btnTheme = document.getElementById("theme-switcher");
-  // document.body.classList.remove('lightmode');
   document.body.classList.add("darkmode");
-  localStorage.setItem("darkMode", "active"); // local storage can only store strings
-  btnTheme.innerHTML = "Light Mode";
+  // localStorage.setItem("darkMode", "active"); // local storage can only store strings
 }
 
 function DisableDarkMode() {
   const btnTheme = document.getElementById("theme-switcher");
   document.body.classList.remove("darkmode");
-  // document.body.classList.add('lightmode');
-  localStorage.setItem("darkMode", null);
-  btnTheme.innerHTML = "Dark Mode";
+  // localStorage.setItem("darkMode", null);
 }
