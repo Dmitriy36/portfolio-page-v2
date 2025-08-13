c91010a86eeb297701f4b58b9772361a6fcc9ad5
Ignatius_Reilly <dmitriy_xix@hotmail.com>
2025-08-13 07:02:00 -0400

    improved dark/light icon

--- script.js
+++ script.js
@@ -7,12 +7,16 @@ function SwitchTheme() {
 function EnableDarkMode() {
   const btnTheme = document.getElementById("theme-switcher");
   document.body.classList.add("darkmode");
+  const imgTheme = document.getElementById("theme-image");
+  imgTheme.src = "images/sun.png";
   // localStorage.setItem("darkMode", "active"); // local storage can only store strings
 }
 
 function DisableDarkMode() {
   const btnTheme = document.getElementById("theme-switcher");
   document.body.classList.remove("darkmode");
+  const imgTheme = document.getElementById("theme-image");
+  imgTheme.src = "images/moon.png";
   // localStorage.setItem("darkMode", null);
 }
 
