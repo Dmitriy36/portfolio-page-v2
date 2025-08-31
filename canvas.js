bbd2468a0f1c8d044194ca12ffd7a40683a71534
Ignatius_Reilly <dmitriy_xix@hotmail.com>
2025-08-31 04:28:10 -0400

    fixed infection!

--- canvas.js
+++ canvas.js
@@ -1,20 +1,26 @@
-// setup
-const container = document.querySelector("container");
+const container = document.querySelector(".container");
 const canvas = document.getElementById("canvas1");
-const ctx = canvas.getContext("2d"); // ctx = context
+const report1 = document.getElementById("reportInfected");
+const context = canvas.getContext("2d");
 
 canvas.width = container.offsetWidth;
 canvas.height = container.offsetWidth;
 
-ctx.fillStyle = "red";
-ctx.strokeStyle = "white";
-ctx.lineWidth = 2;
+const organismsCount = 3000;
+const organismSize = 2;
+let infectionRadius = 3;
+const organismColor = "blue";
+const organismColorInfected = "red";
 
-const organismsCount = 200;
-const organismSize = 3;
+let infectedOrganisms = [];
+let healthyOrganisms = [];
+
+let idGenerator = 0;
 
 class Particle {
   constructor(effect) {
+    this.id = ++idGenerator;
+    this.infected = false;
     this.effect = effect;
     this.radius = organismSize;
     this.x =
@@ -24,19 +30,55 @@ class Particle {
     this.vx = Math.random() * 1 - 0.5;
     this.vy = Math.random() * 1 - 0.5;
   }
+
   draw(context) {
-    context.fillStyle = "hsl(" + this.x * 0.1 + ",100%,50%)";
-    // context.fillStyle = "hsl(" + Math.random() * 360 + ",100%,50%)";
+    if (!this.infected) {
+      context.fillStyle = organismColor;
+    } else {
+      context.fillStyle = organismColorInfected;
+    }
     context.beginPath();
     context.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
     context.fill();
-    // context.stroke();
   }
+
   update() {
     this.x += this.vx;
     if (this.x > this.effect.width || this.x < 0) this.vx *= -1;
     this.y += this.vy;
     if (this.y > this.effect.height || this.y < 0) this.vy *= -1;
+
+    if (this.infected) {
+      this.spreadInfection();
+    }
+  }
+
+  infect() {
+    if (!this.infected) {
+      console.log("an organism has been infected");
+      this.infected = true;
+      infectedOrganisms.push(this);
+      let itemToRemove = healthyOrganisms.indexOf(this);
+      healthyOrganisms.splice(itemToRemove, 1);
+
+      report1.innerHTML = `Infected: ${
+        infectedOrganisms.length
+      } <br/>Healthy: ${healthyOrganisms.length} <br/> Infection Rate: ${(
+        (infectedOrganisms.length / organismsCount) *
+        100
+      ).toFixed(2)}%`;
+    }
+  }
+
+  spreadInfection() {
+    for (let healthy = 0; healthy < healthyOrganisms.length; healthy++) {
+      if (
+        Math.abs(this.x - healthyOrganisms[healthy].x) < infectionRadius &&
+        Math.abs(this.y - healthyOrganisms[healthy].y) < infectionRadius
+      ) {
+        healthyOrganisms[healthy].infect();
+      }
+    }
   }
 }
 
@@ -49,11 +91,15 @@ class Effect {
     this.numberOfParticles = organismsCount;
     this.createParticles();
   }
+
   createParticles() {
     for (let i = 0; i < this.numberOfParticles; i++) {
       this.particles.push(new Particle(this)); // will look for this class and trigger its constructor.
     }
+    healthyOrganisms = [...this.particles];
+    console.log(healthyOrganisms);
   }
+
   handleParticles(context) {
     this.particles.forEach((particle) => {
       particle.draw(context);
@@ -64,9 +110,25 @@ class Effect {
 
 const effect = new Effect(canvas);
 
+function infectOne() {
+  if (healthyOrganisms.length > 1) {
+    let randomOrganism = Math.floor(Math.random() * healthyOrganisms.length);
+    healthyOrganisms[randomOrganism].infect();
+  }
+}
+
+function logInfected() {
+  console.log(infectedOrganisms);
+}
+
+function logHealthy() {
+  console.log(healthyOrganisms);
+}
+
 function animate() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  effect.handleParticles(ctx);
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  effect.handleParticles(context);
   requestAnimationFrame(animate);
 }
+
 animate();
